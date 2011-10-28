aa7ec5c0846b8dbfc64162348f41885ecad877e4
Akira Matsuda <ronnie@dio.jp>
2011-10-28 18:34:18 +0900

    monkey patches for RSpec in order to automatically report after matching

--- lib/monkey/rspec.rb
+++ lib/monkey/rspec.rb
@@ -1,12 +1,39 @@
 module RSpec
-  module Core
-    class Example
-      def inspect
-        ret = RSpec.configuration.reporter.report(1) do |r|
-          self.run ExampleGroup.new, r
+  module Matchers
+    class OperatorMatcher
+      def eval_match_with_reporting(actual, operator, expected)
+        InteractiveRspec.report begin
+          eval_match_without_reporting(actual, operator, expected)
+        rescue RSpec::Expectations::ExpectationNotMetError => err
+          err
         end
-        RSpec.reset
-        ret
+      end
+      alias_method :eval_match_without_reporting, :eval_match
+      alias_method :eval_match, :eval_match_with_reporting
+    end
+  end
+
+  module Expectations
+    class PositiveExpectationHandler
+      class << self
+        unless method_defined? :handle_matcher_with_reporting
+          def handle_matcher_with_reporting(actual, matcher, message=nil, &block)
+            begin
+              result = handle_matcher_without_reporting actual, matcher, message, &block
+              if result.class == TrueClass
+                InteractiveRspec.report result
+              else
+                result
+              end
+            rescue RSpec::Expectations::ExpectationNotMetError => err
+              InteractiveRspec.report err
+              false
+            end
+          end
+
+          alias_method :handle_matcher_without_reporting, :handle_matcher
+          alias_method :handle_matcher, :handle_matcher_with_reporting
+        end
       end
     end
   end
