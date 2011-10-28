5f34ba62b2beb68807fafb405ab9b9882d569e9c
Akira Matsuda <ronnie@dio.jp>
2011-10-28 11:34:35 +0900

    a monkey patch for IRB to start with given context

---
+++ lib/monkey/irb.rb
@@ -0,0 +1,26 @@
+module IRB
+  def IRB.start_with_context(context = nil)
+    IRB.setup nil
+
+    if @CONF[:SCRIPT]
+      irb = Irb.new(nil, @CONF[:SCRIPT])
+    else
+      irb = Irb.new WorkSpace.new(TOPLEVEL_BINDING, context)
+    end
+
+    @CONF[:IRB_RC].call(irb.context) if @CONF[:IRB_RC]
+    @CONF[:MAIN_CONTEXT] = irb.context
+
+    trap("SIGINT") do
+      irb.signal_handle
+    end
+
+    begin
+      catch(:IRB_EXIT) do
+        irb.eval_input
+      end
+    ensure
+      irb_at_exit
+    end
+  end
+end
