7f9c95aed2b79b995d6186190729d26ef89748a5
Akira Matsuda <ronnie@dio.jp>
2011-10-28 17:34:12 +0900

    report function

--- lib/interactive_rspec.rb
+++ lib/interactive_rspec.rb
@@ -1,5 +1,22 @@
-require "interactive_rspec/version"
+require 'interactive_rspec/version'
+require 'rspec'
 
 module InteractiveRspec
-  # Your code goes here...
+  def self.report(result)
+    e = describe.example
+
+    ret = RSpec.configuration.reporter.report(1) do |r|
+      r.instance_variable_set '@example_count', 1
+      if result.is_a? Exception
+        e.send :record, :status => 'failed', :finished_at => Time.now, :run_time => 0, :exception => result
+        r.example_failed e
+        false
+      else
+        r.example_passed e
+        true
+      end
+    end
+    RSpec.reset
+    ret
+  end
 end
